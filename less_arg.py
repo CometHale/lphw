e97a5e59258d0216a34658c9a5121ce2be2b174a
Haley Sanders <haleyfsanders@gmail.com>
2016-03-01 13:10:48 -0800

    updates to notes, added ex13, empty ex4

---
+++ less_arg.py
@@ -0,0 +1,8 @@
+#ex13 Study Drills #2
+from sys import argv
+
+script, name, id_num = argv
+
+print "You just entered %s" %(script)
+print "Your name is %s" %(name)
+print "Your ID is %s" %(id_num)
---
+++ more_arg.py
@@ -0,0 +1,12 @@
+#ex13 Study Drills # 2 and 3
+from sys import argv
+
+script, name, nationality, hometown, state, address = argv
+
+print "You have entered %s, a script within the Twilight Zone." %(script)
+print "Your name is %s." %(name)
+print "You are a citizen of %s." %(nationality)
+print "You're hometown is %s, %s, and you live at %s"%(hometown,state,address)
+scared = raw_input("Are you wondering how I know these things? Are you scared?")
+print "As Halsey would say, 'Damn right you should be scared of me'."
+print "But you also input all of that information yourself so lolz."
